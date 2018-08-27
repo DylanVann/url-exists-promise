df88dd41e07d58f801801bad0a40265ed5eb504a
Dylan Vann <dylan@dylanvann.com>
2018-08-27 16:31:36 -0400

    Handle error.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
-const nodeHttp = require('http')
+const http = require('http')
 const nodeUrl = require('url')
 
 export const urlExists = (url: string): Promise<boolean> =>
-    new Promise(resolve => {
+    new Promise((resolve, reject) => {
         const options = {
             method: 'HEAD',
             host: nodeUrl.parse(url).host,
             port: 80,
             path: nodeUrl.parse(url).pathname,
         }
-        const req = nodeHttp.request(options, (r: { statusCode: number }) => {
+        const req = http.request(options, (r: { statusCode: number }) => {
             resolve(r.statusCode === 200)
         })
+        req.on('error', (e: Error) => {
+            reject(e)
+        });
         req.end()
     })
